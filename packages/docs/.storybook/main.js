20a31bf3f7b4a0d1c2c2daef750c03fdbf9f6c99
leandrotune <leandrotune@pop-os.localdomain>
2023-04-24 20:46:09 -0300

    problem solved

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -13,7 +13,7 @@ module.exports = {
     "builder": "@storybook/builder-vite"
   },
   "features": {
-    "storyStoreV7": true,
+    "storyStoreV7": true
   },
   viteFinal:(config, {configType}) => {
     if (configType === 'PRODUCTION') {
