74e2b2d33410cb776d8ba15e526eb47904a6a0db
leandrotune <leandrotune@pop-os.localdomain>
2023-04-24 22:07:15 -0300

    recreated dependency

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -6,7 +6,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
   ],
   "framework": "@storybook/react",
   "core": {
@@ -15,7 +16,7 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  viteFinal:(config, {configType}) => {
+  viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
       config.base = '/05-design-system/'
     }
